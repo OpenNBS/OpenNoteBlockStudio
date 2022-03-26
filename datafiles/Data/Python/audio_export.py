f7f4e599af3607c2278908a2ec31e441d2fcf3b8
Bentroen <bemcdc@gmail.com>
2022-03-26 15:31:27 -0300

    Format with black + isort

--- datafiles/Data/Python/audio_export.py
+++ datafiles/Data/Python/audio_export.py
@@ -2,21 +2,24 @@ import subprocess
 
 
 def patch_arguments(function, **patched_kwargs):
-     def decorate_name(*args, **kwargs):
-          kwargs.update(patched_kwargs)
-          return function(*args, **kwargs)
-     return decorate_name
+    def decorate_name(*args, **kwargs):
+        kwargs.update(patched_kwargs)
+        return function(*args, **kwargs)
+
+    return decorate_name
 
 
 def main(*args, **kwargs):
-     print("Hello world!")
 
-     # Monkey-patch to avoid ffmpeg/ffprobe calls opening console window
-     subprocess.Popen = patch_arguments(subprocess.Popen, creationflags=subprocess.CREATE_NO_WINDOW)
-     
-     import nbswave
-     nbswave.render_audio(*args, **kwargs)
+    # Monkey-patch to avoid ffmpeg/ffprobe calls opening console window
+    subprocess.Popen = patch_arguments(
+        subprocess.Popen, creationflags=subprocess.CREATE_NO_WINDOW
+    )
+
+    import nbswave
+
+    nbswave.render_audio(*args, **kwargs)
 
 
 if __name__ == "__main__":
-     main()
+    main()
--- datafiles/Data/Python/package.py
+++ datafiles/Data/Python/package.py
@@ -5,22 +5,19 @@ import sys
 import zipfile
 from pathlib import Path
 
-
 PYTHON_VERSION = "3.9.5"
 
 DO_NOT_ADD = [
-     '__pycache__',
-     '_distutils_hack',
-     'pip',
-     'pkg_resources',
-     'setuptools',
-     'wheel',
-     '_virtualenv.py',
+    "__pycache__",
+    "_distutils_hack",
+    "pip",
+    "pkg_resources",
+    "setuptools",
+    "wheel",
+    "_virtualenv.py",
 ]
 
-BLACKLIST = [
-     "numpy" # Special case - can't be loaded from ZIP
-]
+BLACKLIST = ["numpy"]  # Special case - can't be loaded from ZIP
 
 DO_NOT_ADD.extend(BLACKLIST)
 
@@ -32,72 +29,79 @@ ZIP_PATH = Path("Lib", "site-packages.zip")
 
 
 def pack_filter(path):
-     fn = os.path.basename(path)
-     return not (fn in DO_NOT_ADD or '.dist-info' in fn)
+    fn = os.path.basename(path)
+    return not (fn in DO_NOT_ADD or ".dist-info" in fn)
 
-copy_filter = shutil.ignore_patterns('__pycache__')
+
+copy_filter = shutil.ignore_patterns("__pycache__")
 
 
 def main():
 
-     # Check if the Python version is correct
-     py_version = '.'.join(str(s) for s in sys.version_info[:3])
-     if py_version != PYTHON_VERSION:
-          raise RuntimeError(f"This script must be run in Python {PYTHON_VERSION} "
-                             f"(you're running {py_version})")
+    # Check if the Python version is correct
+    py_version = ".".join(str(s) for s in sys.version_info[:3])
+    if py_version != PYTHON_VERSION:
+        raise RuntimeError(
+            f"This script must be run in Python {PYTHON_VERSION} "
+            f"(you're running {py_version})"
+        )
 
-     # Check the script is being run from the right subdirectory
-     cwd = Path.cwd()
-     if not cwd.parts[-3:] == ('datafiles', 'Data', 'Python'):
-          raise RuntimeWarning("This script must be run from the datafiles/Data/Python "
-                               "subfolder in the Open Note Block Studio root directory.")
+    # Check the script is being run from the right subdirectory
+    cwd = Path.cwd()
+    if not cwd.parts[-3:] == ("datafiles", "Data", "Python"):
+        raise RuntimeWarning(
+            "This script must be run from the datafiles/Data/Python "
+            "subfolder in the Open Note Block Studio root directory."
+        )
 
-     if not os.path.exists('.venv'):
-          raise FileNotFoundError("The .venv directory was not found. Have you ran"
-                                  "poetry install before running this script?")
+    if not os.path.exists(".venv"):
+        raise FileNotFoundError(
+            "The .venv directory was not found. Have you ran"
+            "poetry install before running this script?"
+        )
 
-     # Delete previous build directory
-     if Path.exists(OUT_PATH):
-          print("Removing output directory")
-          shutil.rmtree(OUT_PATH)
+    # Delete previous build directory
+    if Path.exists(OUT_PATH):
+        print("Removing output directory")
+        shutil.rmtree(OUT_PATH)
 
-     # Create output directory
-     os.makedirs(OUT_PATH)
+    # Create output directory
+    os.makedirs(OUT_PATH)
 
-     # Package dependencies
-     package_count = 0
-     with zipfile.PyZipFile(ZIP_PATH, mode='w') as zip_module:
-          for path in os.listdir(LIB_PATH):
-               lib_name = os.path.basename(path)
-               lib_path = Path(LIB_PATH, lib_name)
-               try:
-                    zip_module.writepy(lib_path, filterfunc=pack_filter)
-               except RuntimeError:  # only directories or .py files accepted
-                    continue
-               else:
-                    if pack_filter(lib_path):
-                         print(f"Packaging {lib_name}")
-                         package_count += 1
-                    
-     # Handle special cases
-     for lib_name in BLACKLIST:
-          print(f"Packaging {lib_name}")
-          package_count += 1
-          inpath = Path(LIB_PATH, lib_name)
-          outpath = Path(OUT_PATH, lib_name)
-          shutil.copytree(inpath, outpath, ignore=copy_filter)
-          compileall.compile_dir(outpath, force=True, quiet=True, legacy=True)
-          # Remove corresponding .py files
-          for root, _, files in os.walk(outpath):
-               for filename in files:
-                    if filename.endswith('.py'):
-                         os.remove(os.path.join(root, filename))
+    # Package dependencies
+    package_count = 0
+    with zipfile.PyZipFile(ZIP_PATH, mode="w") as zip_module:
+        for path in os.listdir(LIB_PATH):
+            lib_name = os.path.basename(path)
+            lib_path = Path(LIB_PATH, lib_name)
+            try:
+                zip_module.writepy(lib_path, filterfunc=pack_filter)
+            except RuntimeError:  # only directories or .py files accepted
+                continue
+            else:
+                if pack_filter(lib_path):
+                    print(f"Packaging {lib_name}")
+                    package_count += 1
 
-     # Delete virtual environment
-     #shutil.rmtree(ENV_PATH)
+    # Handle special cases
+    for lib_name in BLACKLIST:
+        print(f"Packaging {lib_name}")
+        package_count += 1
+        inpath = Path(LIB_PATH, lib_name)
+        outpath = Path(OUT_PATH, lib_name)
+        shutil.copytree(inpath, outpath, ignore=copy_filter)
+        compileall.compile_dir(outpath, force=True, quiet=True, legacy=True)
+        # Remove corresponding .py files
+        for root, _, files in os.walk(outpath):
+            for filename in files:
+                if filename.endswith(".py"):
+                    os.remove(os.path.join(root, filename))
 
-     print(f"Done! {package_count} packages were added.")
+    # Delete virtual environment
+    # shutil.rmtree(ENV_PATH)
+
+    print(f"Done! {package_count} packages were added.")
 
 
 if __name__ == "__main__":
-     main()
+    main()
